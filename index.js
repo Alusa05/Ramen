9602b2abc4042cd1b14d1816205714949d1f91b8
Lisa Alusa <sayilisaalusa@gmail.com>
2025-03-17 18:42:54 +0300

    aligned the form

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const ramens = [
     { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "Images/shoyu.jpg", rating: 5, comment: "Delicious!" },
+    
     { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "Images/miso.jpg", rating: 4, comment: "Very flavorful!" },
     { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "Images/tonkotsu.jpg", rating: 5, comment: "Rich and creamy!" },
     { id: 4, name: "Gyukotsu Ramen", restaurant: "Ramen-ya", image: "Images/gyukotsu.jpg", rating: 4, comment: "Very refreshing!" },
