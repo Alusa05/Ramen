8ffc5305f59851cca2488d5c811f4152977890a1
Lisa Alusa <sayilisaalusa@gmail.com>
2025-03-17 09:36:49 +0300

    Resizing images

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const ramens = [
-    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "shoyu.jpg", rating: 5, comment: "Delicious!" },
-    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
-    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
+    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image:"Images/shoyu.jpg", rating: 5, comment: "Delicious!" },
+    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "Images/miso.jpg", rating: 4, comment: "Very flavorful!" },
+    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "Images/tonkotsu.jpg" }
  ];
  //Displaying the Ramen Images
  function displayRamens() {
